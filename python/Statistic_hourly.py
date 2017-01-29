f43e2f1da60e0b5e5dcff33371ffe0b35054c98c
desperado <calvin.helloworld@gmail.com>
2017-01-30 00:25:03 +0900

    sanity restore

--- python/Statistic_hourly.py
+++ python/Statistic_hourly.py
@@ -2,61 +2,6 @@ from array import array
 import numpy as np
 import pandas as pd
 from pandas.core.frame import DataFrame
-import math
-
-def get_pivots(data):
-    data['swings'] = np.nan
-
-    pivot = data.irow(0).open
-    last_pivot_id = 0
-    up_down = 0
-
-    diff = .3
-
-    for i in range(0, len(data)):
-        row = data.irow(i)
-
-        # We don't have a trend yet
-        if up_down == 0:
-            if row.low < pivot - diff:
-                data.ix[i, 'swings'] = row.low - pivot
-                pivot, last_pivot_id = row.low, i
-                up_down = -1
-            elif row.high > pivot + diff:
-                data.ix[i, 'swings'] = row.high - pivot
-                pivot, last_pivot_id = row.high, i
-                up_down = 1
-
-        # Current trend is up
-        elif up_down == 1:
-            # If got higher than last pivot, update the swing
-            if row.high > pivot:
-                # Remove the last pivot, as it wasn't a real one
-                data.ix[i, 'swings'] = data.ix[last_pivot_id, 'swings'] + (row.high - data.ix[last_pivot_id, 'high'])
-                data.ix[last_pivot_id, 'swings'] = np.nan
-                pivot, last_pivot_id = row.high, i
-            elif row.low < pivot - diff:
-                data.ix[i, 'swings'] = row.low - pivot
-                pivot, last_pivot_id = row.low, i
-                # Change the trend indicator
-                up_down = -1
-
-        # Current trend is down
-        elif up_down == -1:
-             # If got lower than last pivot, update the swing
-            if row.low < pivot:
-                # Remove the last pivot, as it wasn't a real one
-                data.ix[i, 'swings'] = data.ix[last_pivot_id, 'swings'] + (row.low - data.ix[last_pivot_id, 'low'])
-                data.ix[last_pivot_id, 'swings'] = np.nan
-                pivot, last_pivot_id = row.low, i
-            elif row.high > pivot - diff:
-                data.ix[i, 'swings'] = row.high - pivot
-                pivot, last_pivot_id = row.high, i
-                # Change the trend indicator
-                up_down = 1
-
-    print(data)
-
 
 df=pd.read_csv('D:/workspace/java/fx/files/eurusd/DAT_ASCII_EURUSD_M1_2016.csv', sep=';',names = ["t", "o", "h", "l","c","dummy"])
 
@@ -83,4 +28,4 @@ for i in range(0, days.size):
             l = hourly['l'].min()
             data = np.append(data, [[days[i], j, o, h, c, l]], axis=0)
 
-get_pivots(data)
+print(data)
