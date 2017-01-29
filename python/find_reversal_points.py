fe829c9fa24fdb60886cfbf9e037f88b591ea9ae
desperado <calvin.helloworld@gmail.com>
2017-01-30 00:22:42 +0900

    add plotting

--- python/find_reversal_points.py
+++ python/find_reversal_points.py
@@ -3,9 +3,11 @@ import numpy as np
 import pandas as pd
 from pandas.core.frame import DataFrame
 import math
+from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as md
 
 def get_pivots(data):
-    data['swings'] = np.nan
 
     pivot = data.iloc[0].open
     offset = data[data['time'] ==  data.iloc[0].time].index.values.astype(int)[0]
@@ -17,17 +19,17 @@ def get_pivots(data):
 
     diff = .002
 
-    for i in range(0, len(data)):
-        row = data.iloc[i]
+    for i in range(offset, len(data)+offset):
+        row = data.loc[i]
 
         # We don't have a trend yet
         if up_down == 0:
             if row.low < pivot - diff:
-                data.ix[i + offset, 'swings'] = row.low - pivot
+                data.ix[i, 'swings'] = row.low - pivot
                 pivot, last_pivot_id = row.low, i
                 up_down = -1
             elif row.high > pivot + diff:
-                data.ix[i + offset, 'swings'] = row.high - pivot
+                data.ix[i, 'swings'] = row.high - pivot
                 pivot, last_pivot_id = row.high, i
                 up_down = 1
 
@@ -36,11 +38,11 @@ def get_pivots(data):
             # If got higher than last pivot, update the swing
             if row.high > pivot:
                 # Remove the last pivot, as it wasn't a real one
-                data.ix[i + offset, 'swings'] = data.ix[last_pivot_id + offset, 'swings'] + (row.high - data.ix[last_pivot_id + offset, 'high'])
-                data.ix[last_pivot_id + offset, 'swings'] = np.nan
+                data.ix[i, 'swings'] = data.ix[last_pivot_id, 'swings'] + (row.high - data.ix[last_pivot_id, 'high'])
+                data.ix[last_pivot_id, 'swings'] = np.nan
                 pivot, last_pivot_id = row.high, i
-            elif row.low < pivot - diff:
-                data.ix[i + offset, 'swings'] = row.low - pivot
+            elif ((pivot-row.low)/diff) > 1:
+                data.ix[i, 'swings'] = row.low - pivot
                 pivot, last_pivot_id = row.low, i
                 # Change the trend indicator
                 up_down = -1
@@ -50,22 +52,40 @@ def get_pivots(data):
              # If got lower than last pivot, update the swing
             if row.low < pivot:
                 # Remove the last pivot, as it wasn't a real one
-                data.ix[i + offset, 'swings'] = data.ix[last_pivot_id + offset, 'swings'] + (row.low - data.ix[last_pivot_id + offset, 'low'])
-                data.ix[last_pivot_id + offset, 'swings'] = np.nan
+                data.ix[i, 'swings'] = data.ix[last_pivot_id, 'swings'] + (row.low - data.ix[last_pivot_id, 'low'])
+                data.ix[last_pivot_id, 'swings'] = np.nan
                 pivot, last_pivot_id = row.low, i
-            elif row.high > pivot - diff:
-                data.ix[i + offset, 'swings'] = row.high - pivot
+            elif ((row.high-pivot)/diff) > 1:
+                data.ix[i, 'swings'] = row.high - pivot
                 pivot, last_pivot_id = row.high, i
                 # Change the trend indicator
                 up_down = 1
 
+
+    minutes = data['time'].apply(lambda t: datetime.strptime(t, '%Y%m%d %H%M00')) 
+    #minutes = matplotlib.dates.date2num(minutes)
     
+    ax=plt.subplot()
+    
+    ax.xaxis_date()
+    ax.xaxis.set_major_formatter(md.DateFormatter('%H:%M'))
+
+    plt.plot_date(minutes, data['close'])
+    plt.show()
     print(data[data['swings'].notnull()])
 
 
 df = pd.read_csv('D:/workspace/java/fx/files/eurusd/DAT_ASCII_EURUSD_M1_2016.csv', sep=';',names = ["time", "open", "high", "low","close","dummy"])
+df['swings'] = np.nan
 df = df.drop(labels='dummy', axis=1)
 
-df = df[(df['time'] > '20161212 0629') & (df['time'] < '20161212 21')]
+dates = df['time'].apply(lambda x: x[:8]).unique()
 
-get_pivots(df)
+for i in range(0, dates.size):
+    dt = datetime.strptime(dates[i], '%Y%m%d')
+    data = df[(df['time'] > dt.strftime('%Y%m%d 0630')) & (df['time'] < dt.strftime('%Y%m%d 2100'))] # active time
+    print()
+   
+    get_pivots(data)
+
+
